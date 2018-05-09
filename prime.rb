5af42e55d7d28933bd0d02b3bf7826c6573bb663
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-09 03:44:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+
+def prime?(integer)
+primes = []
+n = 2
+if integer < n
+ return false
+else n <= integer
+(2...integer).each |x|
+ if (integer % x) == 0
+ n++
+ else
+   return true
+ end 
+ end
+if primes.include?(integer) == true
+return "true"
+else 
+return "false"
+end
+end
